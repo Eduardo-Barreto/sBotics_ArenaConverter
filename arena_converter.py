04e37cae6ef6113c8418fe74178867ed2dc2dcb6
Eduardo Santos Barreto <eduwagzozo@gmail.com>
2021-04-11 01:45:14 -0300

    conversor para json adicionado

--- arena_converter.py
+++ arena_converter.py
@@ -1,8 +1,9 @@
-import os
 from data import *
 
-def converter_arena(arena_antiga):
+
+def antigo_padrao2(arena_antiga):
     arena_antiga = arena_antiga.strip('\n')
+
     arena_antiga = arena_antiga.split(',')
     arena_final = ''
 
@@ -16,8 +17,8 @@ def converter_arena(arena_antiga):
                 xy = x[::-1] + y[::-1]
                 tipo = peca[4:5]
                 identificador = peca[5:6]
-                if identificador == ')':
-                  continue
+                if identificador == '(':
+                    continue
                 id_convertido = ids.get(identificador)
                 rotacao = peca[6:]
                 if rotacao == '000':
@@ -65,9 +66,72 @@ def converter_arena(arena_antiga):
     arena_final += config_adicional_padrao
     return arena_final
 
-print('Cole o código da arena antiga:')
-arena_antiga = input()
-arena_convertida = converter_arena(arena_antiga)
-os.system('clear')
-print('Arena convertida:\n')
-print(arena_convertida)
+
+def padrao2_json(arena_antiga):
+    arena_antiga = arena_antiga.strip('\n')
+    arena_antiga = arena_antiga.split(',')
+    arena_final = {}
+    tiles = []
+    settings = {}
+    rescue_zone = {'ramp': 2}
+    for parte in arena_antiga:
+        parte = parte.strip()
+        if parte[0].isnumeric():
+            if len(parte) > 2:
+                peca = parte
+                x = peca[0:2]
+                if x.startswith('0'):
+                    x = x[1:]
+                z = peca[2:4]
+                if z.startswith('0'):
+                    z = z[1:]
+                tipo = peca[4:5]
+                if tipo == 'r':
+                    tipo = 'straight_line'
+                else:
+                    tipo = 'curve'
+                identificador = peca[5:6]
+                if identificador == '(':
+                    continue
+                id_convertido = ids.get(identificador)
+                rotacao = peca[6:7]
+                altura = peca[7:8]
+                tile = {'id': id_convertido, 'x': x, 'z': z, 'y': altura, 'type': tipo,
+                        'rotation': rotacao, 'properties': {'has_checkpoint': False}}
+                tiles.append(tile)
+            else:
+                tipo_resgate = parte
+                rescue_zone.update({'ramp': tipo_resgate})
+        else:
+            config_antiga = parte
+            config_antiga = config_antiga.split(': ')
+            if config_antiga[0] in configs:
+                config_nova = configs.get(config_antiga[0])
+            elif config_antiga[0] in configs_resgate:
+                config_nova = configs_resgate.get(config_antiga[0])
+                if config_nova not in rescue_zone:
+                    rescue_zone.update({config_nova: config_antiga[1]})
+                continue
+            else:
+                if config_antiga[0] == 'RobosPerm':
+                    robos_permitidos = []
+                    for caractere in config_antiga[1]:
+                        if caractere == '0':
+                            robos_permitidos.append(False)
+                        else:
+                            robos_permitidos.append(True)
+                    settings.update(
+                        {'robot_permissions': {
+                            'robot_one': robos_permitidos[0],
+                            'robot_two': robos_permitidos[1],
+                            'robot_three': robos_permitidos[2],
+                            'robot_four': robos_permitidos[3],
+                            'robot_five': robos_permitidos[4],
+                            'custom_robots': robos_permitidos[5]
+                        }}
+                    )
+
+    settings.update({'rescue_zone': rescue_zone})
+    arena_final.update({'tiles': tiles})
+    arena_final.update({'settings': settings})
+    return str(arena_final).replace('\'', '"').lower()
